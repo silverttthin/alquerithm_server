a73f7484706230547fb183438afec9e606b54d9e
leesw <silverttthin@naver.com>
2024-07-08 17:57:46 +0900

    리포지토리 보내기 전 최종 백업

--- models.py
+++ models.py
@@ -1,28 +1,40 @@
-from pydantic import BaseModel, BeforeValidator, Field, ConfigDict
+from pydantic import BaseModel, BeforeValidator, Field
 from typing import Annotated, List, Optional
 
 pyObjectId = Annotated[str, BeforeValidator(str)]
 
 
+class CommentModel(BaseModel):
+    id: Optional[pyObjectId] = Field(alias="_id", default=None)
+    content: str
+    likes: int
+    post_id: Optional[pyObjectId] = Field(alias="post_id", default=None) # 이 댓글이 달린 게시글 정보
+    author_id: Optional[pyObjectId] = Field(alias="author_id", default=None) # 이 댓글을 쓴 놈 정보
+
+
 class PostModel(BaseModel):
     id: Optional[pyObjectId] = Field(alias="_id", default=None)
-    title: str
+    problemNum: str
+    codeURL: str
     content: str
     result: str
-    author_id: str
-    model_config = ConfigDict(
-        populate_by_name=True,
-        arbitrary_types_allowed=True,
-        json_schema_extra={
-            "example": {
-                "title": "1000",
-                "content": "1000번 문제 루비급 아님?",
-                "result": "질문",
-                "author_id": "123"
-            }
-        },
-    )
+    author_id: Optional[pyObjectId] = Field(alias="author_id", default=None)
+    comments: List[CommentModel] = []
+
+
+class UserModel(BaseModel):
+    id: Optional[pyObjectId] = Field(alias="_id", default=None)
+    username: str
+    tags: List[str]
+    alias_num: int
+
+    my_posts: List[PostModel] = []
+    commented_posts: List[CommentModel] = []
+    # boormark는 좀 나중에
 
 
 class PostCollection(BaseModel):
     posts: List[PostModel]
+
+
+
